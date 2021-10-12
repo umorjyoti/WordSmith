83b7bb6e8f8e67e8c792600b617611c235cdefa9
Umorjyoti Chetia <umorjyotichetia@gmail.com>
2021-10-12 11:29:29 +0530

    host 2021

--- InkSlinger/settings.py
+++ InkSlinger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l2(6#03dpa#uz0d=v7$txd2)*((sc%e(&00$(s@uu5u=8chs9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- InkSlinger/urls.py
+++ InkSlinger/urls.py
@@ -2,9 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from post import views
+from django.views.static import serve
+from django.conf.urls import url 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('account/',include('account.urls')),
     path('post/',include('post.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
