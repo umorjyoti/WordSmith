05c85bf02ca3f251d0322fa381591e9b032b2163
umorjyoti <umorjyotichetia@gmail.com>
2021-10-12 12:23:26 +0530

    depliy

--- InkSlinger/settings.py
+++ InkSlinger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,4 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+django_heroku.settings(locals())
